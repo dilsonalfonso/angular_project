43041d8ae2e95fb4115af35f98f3949ef7bed483
dilsonalfonso <dilson.05.09.da@gmail.com>
2024-05-07 23:36:42 -0500

    Inicio de Angular y Rutas creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Components
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+
+export const routes: Routes = [
+    { path: '', component: ListarProductosComponent},
+    { path: 'crear-producto', component: CrearProductoComponent},
+    { path: 'editar-producto/:id', component: CrearProductoComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+
+
+];
